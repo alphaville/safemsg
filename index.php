109fe06e3a0f7e9fa376e9d6dfd3da88f19f8eb1
Pantelis Sopasakis <pantelis.sopasakis@imtlucca.it>
2014-12-23 15:36:13 +0100

    Fixed bugs found by A. Themelis

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
-<html>
-<head>
-	<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
-	<meta content="utf-8" http-equiv="encoding">
-	<?php http_response_code(403); ?>
-</head>
-<body>
-<h1>Forbidden - 403</h1>
-</body>
-</html>
+<?php
+	header("Location: http://".$_SERVER["SERVER_NAME"]."/safemsg/create.php");
+	die();
+?>
